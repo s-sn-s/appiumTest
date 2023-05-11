ab030cbaf072dea9113819f74c50d1b85db22bc3
s-sn-s <savin.sn.shetty@gmail.com>
2023-05-11 23:58:26 +0530

    lets w3c scroll

--- com.srn.testaid/src/test/java/com/srn/testaid/ScrollMe.java
+++ com.srn.testaid/src/test/java/com/srn/testaid/ScrollMe.java
@@ -9,16 +9,13 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.PointerInput;
 import org.openqa.selenium.interactions.PointerInput.Kind;
 import org.openqa.selenium.interactions.Sequence;
-//import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-
 import io.appium.java_client.AppiumBy;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
-import io.appium.java_client.android.AndroidTouchAction;
 
 public class ScrollMe {
 	
@@ -26,12 +23,12 @@ public class ScrollMe {
 	
 	@BeforeTest
 	public void setup() throws MalformedURLException {
-		//*******Deprecated
-//		DesiredCapabilities caps = new DesiredCapabilities();
-//		caps.setCapability("platformName", "Android");
-//		caps.setCapability("appium:automationName", "UiAutomator2");
-//		caps.setCapability("app", System.getProperty("user.dir")+"/apps/ApiDemos-debug.apk");
-		//Deprecated*******
+		/**************Deprecated**************
+		DesiredCapabilities caps = new DesiredCapabilities();
+		caps.setCapability("platformName", "Android");
+		caps.setCapability("appium:automationName", "UiAutomator2");
+		caps.setCapability("app", System.getProperty("user.dir")+"/apps/ApiDemos-debug.apk");
+		***************Deprecated**************/
 		UiAutomator2Options opts = new UiAutomator2Options();
 		opts.setCapability("app", System.getProperty("user.dir")+"/apps/ApiDemos-debug.apk");
 		a = new AndroidDriver(new URL("http://localhost:4723"),opts);
@@ -58,26 +55,31 @@ public class ScrollMe {
 		
 		swipe.addAction(f.createPointerDown(0));
 		
-		swipe.addAction(f.createPointerMove(Duration.ofSeconds(0),PointerInput.Origin.viewport(),(int)centerx,(int)endy));
+		swipe.addAction(f.createPointerMove(Duration.ofSeconds(1),PointerInput.Origin.viewport(),(int)centerx,(int)endy));
 		
 		swipe.addAction(f.createPointerUp(0));
 		
 		a.perform(Arrays.asList(swipe));
+		a.perform(Arrays.asList(swipe));
 		Thread.sleep(2000);
+		
 		a.findElement(AppiumBy.accessibilityId("Tabs")).click();
+		
 		a.findElement(AppiumBy.accessibilityId("1. Content By Id")).click();
-		a.findElement(AppiumBy.xpath("//*[text()='TAB1']")).click();
+		
+		//success
+		//a.findElement(AppiumBy.xpath("//*[@text='TAB1']")).click();
 		String t1 = a.findElement(AppiumBy.accessibilityId("tab1")).getText();
 		System.out.println("I clicked "+ t1);
-		a.findElement(AppiumBy.xpath("//*[@index='0']")).click();
+		a.findElement(AppiumBy.xpath("//*[@class='android.widget.LinearLayout'][2]")).click();
+		Thread.sleep(2000);
 		String t2 = a.findElement(AppiumBy.accessibilityId("tab2")).getText();
 		System.out.println("I clicked "+ t2);
-		a.findElement(AppiumBy.xpath("text()='TAB3'")).click();
+		a.findElement(AppiumBy.xpath("//*[@text='TAB3']")).click();
 		String t3 = a.findElement(AppiumBy.accessibilityId("tab3")).getText();
 		System.out.println("I clicked "+ t3);
 		Thread.sleep(2000);
 	}
-	  
 	
 	@AfterTest
 	public void tearDown() {
---
+++ com.srn.testaid/src/test/java/com/srn/testaid/scrollTest.java
@@ -0,0 +1,71 @@
+package com.srn.testaid;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+import java.util.Arrays;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.PointerInput;
+import org.openqa.selenium.interactions.Sequence;
+import org.openqa.selenium.interactions.PointerInput.Kind;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+
+public class scrollTest {
+	
+	AppiumDriver a;
+	
+	@BeforeTest
+	public void driveSetup() throws MalformedURLException {
+		UiAutomator2Options ua = new UiAutomator2Options();
+		ua.setCapability("app", System.getProperty("user.dir")+"/apps/ApiDemos-debug.apk");
+		a = new AndroidDriver(new URL("http://localhost:4723"),ua);
+	}
+	
+	@Test
+	public void testScrollToTab() throws InterruptedException {
+a.findElement(AppiumBy.accessibilityId("Views")).click();
+		
+		WebElement VSlist = a.findElement(AppiumBy.id("android:id/list"));
+		
+		double centerx = VSlist.getRect().x + (VSlist.getSize().width/2);
+		
+		double starty = VSlist.getRect().y + (VSlist.getSize().height*0.9);
+		
+		double endy = VSlist.getRect().y + (VSlist.getSize().height*0.1);
+		
+		//finger 
+		PointerInput f = new PointerInput(Kind.TOUCH, "f");
+		
+		Sequence swipe = new Sequence(f, 1);
+		
+		swipe.addAction(f.createPointerMove(Duration.ofSeconds(0),PointerInput.Origin.viewport(),(int)centerx,(int)starty));
+		
+		swipe.addAction(f.createPointerDown(0));
+		
+		swipe.addAction(f.createPointerMove(Duration.ofSeconds(1),PointerInput.Origin.viewport(),(int)centerx,(int)endy));
+		
+		swipe.addAction(f.createPointerUp(0));
+		
+		a.perform(Arrays.asList(swipe));
+		a.perform(Arrays.asList(swipe));
+		
+		a.findElement(AppiumBy.accessibilityId("TextSwitcher")).click();
+		a.findElement(AppiumBy.accessibilityId("Next")).click();
+		Thread.sleep(2000);
+	}
+	
+	@AfterTest
+	public void tearDown() {
+		a.quit();
+	}
+	
+	
+}
